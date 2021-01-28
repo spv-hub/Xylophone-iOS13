b8c1731f06a6199873987fe54714d6ace256e7b5
Sandy <sandyvasquez9@gmail.com>
2021-01-28 01:19:04 -0800

    added opacity and delay to app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,17 +5,35 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's button that got pressed to half opacity
+        sender.alpha = 0.5
+        
+//  how to delay actions in Swift https:stackoverflow.com/questions/27085009/delay-actions-in-swift
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2) ) {
+//Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 }
-
